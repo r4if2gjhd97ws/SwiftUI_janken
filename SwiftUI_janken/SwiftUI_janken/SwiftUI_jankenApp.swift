7880872f12e58fb9a2c2477a7e0949bd4b52b976
yohei_takasaki <y.takasaki@speakbuddy.jp>
2022-10-29 22:58:24 +0900

    プロジェクト作成

---
+++ SwiftUI_janken/SwiftUI_janken/SwiftUI_jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_jankenApp.swift
+//  SwiftUI_janken
+//
+//  Created by ようへい on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
